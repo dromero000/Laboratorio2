2fe2980965da77be9d27afde4868a7c542f55418
dromero000 <d.romero@unitec.edu>
2022-01-28 14:19:49 -0600

    Creación clase de apoyo

--- src/Lab2P2_DanaRomero_22141150/Animal.java
+++ src/Lab2P2_DanaRomero_22141150/Animal.java
@@ -11,4 +11,30 @@ package Lab2P2_DanaRomero_22141150;
  */
 public class Animal {
     
+    //Atributos
+    String nombreCient,nombreComun, habitat, alimento, rasgos, distGeo;
+    int vida;
+    
+    //Constructor
+    public Animal(String nombreCient, String nombreComun, String habitat, String alimento, String rasgos, String distGeo, int vida){
+        this.nombreCient=nombreCient;
+        this.nombreComun = nombreComun;
+        this.habitat = habitat;
+        this.alimento = alimento;
+        this.rasgos=rasgos;
+        this.distGeo=distGeo;
+        this.vida = vida;                
+    }
+    
+    //Funciones
+    
+    //Función para imprimir los atributos
+    public void imprimir(){
+        System.out.println("[Nombre Científico: "+ nombreCient+", Nombre Común: "+ nombreComun+", Hábitat: "+
+                habitat+", Alimentación: "+alimento+", Descripción de Rasgos: "+rasgos+", Distribución Geográfica: "+
+                distGeo+", Vida: "+vida);
+    }
+    
+    
+    
 }
--- src/Lab2P2_DanaRomero_22141150/DiscoveryMain.java
+++ src/Lab2P2_DanaRomero_22141150/DiscoveryMain.java
@@ -5,10 +5,40 @@
  */
 package Lab2P2_DanaRomero_22141150;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Dana Romero
  */
 public class DiscoveryMain {
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        
+        
+        
+        System.out.println("* * * R E G I S T R O  A N I M A L E S * * *");
+        
+        int opcion;
+        
+        do{
+            System.out.println("---M E N Ú---");
+            System.out.println("1 - Registrar\n2 - Eliminar\n3 - Editar");
+            System.out.print("Seleccione una opción: ");
+            opcion = leer.nextInt();
+            switch(opcion){
+                case 1:
+                    System.out.println("---Registrar---");
+                    System.out.println("Ingrese nombre científico: ");
+                    String nombreCient = leer.nextLine();
+                    break;
+                case 2:
+                    break;
+            }
+            
+        }while (opcion!=5);
+    }
     
 }
