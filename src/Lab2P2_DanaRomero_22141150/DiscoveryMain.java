ce9b3370d134c74cce52c41ae202c767fea82550
dromero000 <d.romero@unitec.edu>
2022-01-28 13:40:26 -0600

    Creación clase Main

---
+++ src/Lab2P2_DanaRomero_22141150/DiscoveryMain.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab2P2_DanaRomero_22141150;
+
+/**
+ *
+ * @author Dana Romero
+ */
+public class DiscoveryMain {
+    
+}
